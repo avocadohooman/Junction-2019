3bbc0ff75c125b035f590f4477d3cd912f79a067
Gerhard Molin <gerhard@minimumbadass.com>
2019-11-16 19:15:37 +0200

    updated source code

--- source.c
+++ source.c
@@ -1,7 +1,60 @@
 #include <stdio.h>
-
+#include <time.h>
+void delay(int number_of_seconds) 
+{ 
+    // Converting time into milli_seconds 
+    int milli_seconds = 1000 * number_of_seconds; 
+    // Stroing start time 
+    clock_t start_time = clock(); 
+    // looping till required time is not acheived 
+    while (clock() < start_time + milli_seconds) 
+        ; 
+} 
 int main()
 {
-    printf("This is a source code 01010101010101");
+    int i;
+    printf("Generating source code...\n");
+    delay(1000);
+    delay(1000);
+    delay(1000);
+    delay(1000);
+    printf("Compiling source code...\n");
+    delay(1000);
+    delay(1000);
+    delay(1000);
+    printf("Souce code ready\n");
+    delay(1000);
+    printf("Initializing stack\n");
+    delay(1000);
+    printf("Please wait.\n");
+    delay(1000);
+    i = 10;
+    while (i > 0)
+    {
+        printf("Estimated time left: %d seconds\n", i);
+        delay(1000); 
+        --i;
+    }
+    printf("Source code ready\n");
+    delay(1000);
+    delay(1000);
+    printf("\n$$========$$ TOP SECRET $$========$$\n\n");
+    printf("This is the source code: 01010101010101\n");
+    printf("\n$$========$$ TOP SECRET $$========$$\n");
+    delay(1000);
+    printf("\nOperation Successful\n");
+    delay(1000);
+    printf("\nUNAUTHORIZED ACCESS KEY\n");
+    delay(1000);
+    printf("\nSELF DELETION SEQUENCE STARTED\n");
+    delay(1000);
+    i = 10;
+    while (i > 0)
+    {
+        printf("Time left until self deletion: %d seconds\n", i);
+        delay(1000); 
+        --i;
+    }
+    printf("\e[1;1H\e[2J");
     return (0);
 }
